68775723e63f6cdfbf1eb86dd7d62bd99a4fd6ba
Cliff <134680422+Finn9928@users.noreply.github.com>
2024-03-07 21:12:28 +1300

    Update 0.0.1 UndUnl.js
    added the player sprite

--- UndUnl.js
+++ UndUnl.js
@@ -1,30 +1,51 @@
 /*******************************************************/
-// App Name: Undead Unleashed
+// Game Name: Undead Unleashed
 // 
 // Written by Cliff Harfield
 /*******************************************************/
 console.log('Boot UndUnl');
+// Seting up the variable and constants etc
+var player;
 let gameState='game';
+const VERSION_NUM = '0.0.1'
+const PLAYER_SCALE = 30;
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
+    console.log(" setup: Undead Unleashed");
     new Canvas(windowWidth, windowHeight);
-    const VERSION_NUM = '0.0.1'
+    resetGame();
 }
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
+    if (gameState == 'startScreen') {
+        startScreen ();
+    }
     if (gameState == 'game') {
         game ();
     }
 }
 
 /*******************************************************/
-// game()
+// functions
 /*******************************************************/
+//game function
 function game() {
     background('green');
 }
+//startscreenfunction
+function startScreen () {
+    background('blue');
+}
+//this is the reset game function
+function resetGame() {
+    player = new Sprite(windowWidth/2, windowHeight/2, PLAYER_SCALE, PLAYER_SCALE, 'k')
+    
+}
+/*******************************************************/
+//  END OF GAME
+/*******************************************************/
