f70ad0df67b8f8809bf710d25f3b8b8497a0f6b6
Cliff <134680422+Finn9928@users.noreply.github.com>
2024-03-13 13:31:12 +1300

    Update 0.1.1 UndUnl.js
    added basic zombies

--- UndUnl.js
+++ UndUnl.js
@@ -8,8 +8,9 @@ console.log('Boot UndUnl');
 var player;
 var playerColour = '#7922E9';
 var playerWeapon;
+var swingSpeed = 6;
 let gameState='game';
-const VERSION_NUM = '0.1.0'
+const VERSION_NUM = '0.1.1'
 const PLAYER_SCALE = 30;
 const PLAYER_SPEED = 4;
 
@@ -33,6 +34,7 @@ function setup() {
     wallTop.color = 'black';
     wallBot = new Sprite(width/2, height, width, 0, 'k');
     wallBot.color = 'black';
+    horedGroup = new Group();
     //reseting the game to start
     resetGame();
 }
@@ -58,12 +60,8 @@ function game() {
     background('green');
     camera.pos = player.pos;
     player.rotateMinTo(mouse, 9, 90);//speed 8 found from testing and 90 is to point the sprite the correct ways
+    playerWeapon.rotateMinTo(mouse, swingSpeed, 90);
     controlsForPlayer ();
-    if (playerWeapon.vel.x > 3||playerWeapon.vel.y > 3||playerWeapon.vel.x < -3||playerWeapon.vel.y < -3) console.log('moving speed ' +(playerWeapon.vel.y+playerWeapon.vel.x));
-    //playerWeapon.pos.y = (player.pos.y + 10);
-    //playerWeapon.pos.x = (player.pos.x + 10);
-    j = new WheelJoint(player, playerWeapon);
-    playerWeapon.rotateMinTo(mouse, 6, 90);
 }
 //startscreenfunction
 function startScreen () {
@@ -84,6 +82,12 @@ function resetGame() {
     playerWeapon.offset.y = -50;
     playerWeapon.addImage(playerSwordTexure);
     playerSwordTexure.resize(PLAYER_SCALE, PLAYER_SCALE*2);
+    //the connection point between the player and its sword
+    playerHands = new WheelJoint(playerWeapon, player);
+    playerHands.visible = 'false';
+    playerHands.springiness = 0.01;
+    //kill zombie detection
+    horedGroup.collides(playerWeapon, swordHitZombie);
 }
 //movement code
 function controlsForPlayer () {
@@ -93,26 +97,52 @@ function controlsForPlayer () {
     }
     if (kb.released('a')){
         player.vel.x=0;
+        playerWeapon.vel.x=0;
     }
     if (kb.pressing('d')){
         player.vel.x=+PLAYER_SPEED;
     }
     if (kb.released('d')){
         player.vel.x=0;
+        playerWeapon.vel.x=0;
     }
     if (kb.pressing('w')){
         player.vel.y=-PLAYER_SPEED;
     }
     if (kb.released('w')){
         player.vel.y=0;
+        playerWeapon.vel.y=0;
     }
     if (kb.pressing('s')){
         player.vel.y=+PLAYER_SPEED; 
     }
     if (kb.released('s')){
         player.vel.y=0;
+        playerWeapon.vel.y=0;
+    }
+    //Weapon controls
+    if (mouse.pressing()) swingSpeed = 20;
+    if (mouse.released()) swingSpeed = 6;
+    
+    //texting button
+    if (kb.presses('1')) spawnZombies(1);
+    if (kb.presses('2')) spawnZombies(5);
+}
+function spawnZombies (_amount){
+    for (i = 0; i < _amount; i++) {
+        var randNum1 = random(6, 200);
+        var randNum2 = random(60, 80);
+        var randNum3 = random(14, 24);
+        zombie = new Sprite(randNum1, randNum2, randNum3, "d");
+        zombie.color= "darkGreen";
+        horedGroup.add(zombie);
     }
 }
+function swordHitZombie(_zombie, _player) {
+        console.log("test");
+        // Delete the zombieh was hit
+        _zombie.remove();
+    }
 /*******************************************************/
 //  END OF GAME
 /*******************************************************/
